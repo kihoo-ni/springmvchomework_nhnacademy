f8514a1c7599a206af5824bb55b4076903bb2bef
kihoo-ni <gudcjf532@gmail.com>
2024-05-21 09:06:40 +0900

    로그인필터 구현완료

--- src/main/java/com/nhnacademy/student_info/StudentInfoApplication.java
+++ src/main/java/com/nhnacademy/student_info/StudentInfoApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.student_info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class StudentInfoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/nhnacademy/student_info/filter/LoginCheckFilter.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.student_info.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(
+        filterName = "LoginCheckFilter",
+        urlPatterns = {"/student/modify/*","/student/view/*" }
+)
+public class LoginCheckFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        boolean sessionCookieExists = false;
+        Cookie[] cookies = req.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("SESSION".equals(cookie.getName())) {
+                    sessionCookieExists = true;
+                    break;
+                }
+            }
+        }
+
+        if (!sessionCookieExists) {
+            res.sendRedirect("/login");
+            return;
+        }
+
+        chain.doFilter(req, res);
+    }
+}
