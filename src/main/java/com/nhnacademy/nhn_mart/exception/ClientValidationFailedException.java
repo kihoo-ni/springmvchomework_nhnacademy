a7e934067dde4d5d0b93f1243bff2ea8742e9d30
kihoo-ni <gudcjf532@gmail.com>
2024-05-27 07:49:51 +0900

    5/27 기능 완성 에러처리 완성 테스트 미완성

--- src/main/java/com/nhnacademy/nhn_mart/exception/ClientValidationFailedException.java
+++ src/main/java/com/nhnacademy/nhn_mart/exception/ClientValidationFailedException.java
@@ -8,7 +8,7 @@ public class ClientValidationFailedException extends RuntimeException {
     public ClientValidationFailedException(BindingResult bindingResult) {
         super(bindingResult.getAllErrors()
                 .stream()
-                .map(error -> new StringBuilder().append("필드값=").append(error.getObjectName())
+                .map(error -> new StringBuilder().append("밸리데이션이름=").append(error.getObjectName())
                         .append(",메시지=").append(error.getDefaultMessage())
                         .append(",오류코드=").append(error.getCode()))
                 .collect(Collectors.joining(" | ")));
